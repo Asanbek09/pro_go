4d9b39bddaf681315a9f3e5e25f25dcf525893bf
Asanbek09 <samuidnua@gmail.com>
2024-11-08 12:47:52 +0600

    Использование горути и каналов

--- Chapter_01/14/project/main.go
+++ Chapter_01/14/project/main.go
@@ -1,9 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"time"
+)
+
+func receiveDispatches(channel <-chan DispatchNotification) {
+	for details := range channel {
+		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+	}
+	fmt.Println("Channel has been closed")
+}
 
 func main() {
+	dispatchChannel := make(chan DispatchNotification, 100)
+	//var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
+	//var receiveDispatches <-chan DispatchNotification = dispatchChannel
+	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
+	receiveDispatches((<-chan DispatchNotification)(dispatchChannel))
+
+	/*
+	for details := range dispatchChannel {
+		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+	}
+	*/
+	//fmt.Println("Channel has been closed")
+
+	/*
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrders(dispatchChannel)
+	for {
+		if details, open := <- dispatchChannel; open {
+			fmt.Println("Dispatch to:", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+		} else {
+			fmt.Println("Channel has been closed")
+			break
+		}
+	}
+	*/
+
+	/*
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
+	//time.Sleep(time.Second * 5)
 	fmt.Println("main function completed")
+	*/
 }
--- Chapter_01/14/project/operations.go
+++ Chapter_01/14/project/operations.go
@@ -1,19 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
+	var channel chan float64 = make(chan float64, 2)
 	for category, group := range data {
-		storeTotal += group.TotalPrice(category)
+		go group.TotalPrice(category, channel)
+		//storeTotal += group.TotalPrice(category)
+	}
+	time.Sleep(time.Second * 5)
+	fmt.Println("-- Starting to receive from channel")
+	for i := 0; i < len(data); i++ {
+		//fmt.Println(len(channel), cap(channel))
+		fmt.Println("-- channel read pending", len(channel), "items in buffer, size ", cap(channel))
+		value := <-channel
+		fmt.Println("--channel read complere", value)
+		storeTotal += value
+		time.Sleep(time.Second)
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
 
+func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
+	var total float64
+	for _, p := range group {
+		//fmt.Println(category, "product:", p.Name)
+		total += p.Price
+		time.Sleep(time.Millisecond * 100)
+	}
+	fmt.Println(category, "subtotal:", ToCurrency(total))
+	resultChannel <- total
+	fmt.Println(category, "channel send complete")
+}
+
+/*
 func (group ProductGroup) TotalPrice(category string) (total float64) {
 	for _, p := range group {
+		fmt.Println(category, "product:", p.Name)
 		total += p.Price
+		time.Sleep(time.Millisecond * 100)
 	}
 	fmt.Println(category, "subtotal:", ToCurrency(total))
 	return
 }
+*/
