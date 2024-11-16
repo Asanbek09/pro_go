98be921c4e1a5f1febf6be6c91c15d53db42ee3b
Asanbek09 <samuidnua@gmail.com>
2024-11-16 12:24:57 +0600

    Форматирование и сканирование строк

--- Chapter_02/17/project/main.go
+++ Chapter_02/17/project/main.go
@@ -20,10 +20,40 @@ func getProductName(index int) (name string, err error) {
 */
 
 func main() {
+	var name string
+	var category string
+	var price float64
+
+	source := "Product jakcet watersports 264.59"
+	template := "Product %s %s %f"
+	n, err := fmt.Sscanf(source, template, &name, &category, &price)
+
+	//fmt.Print("Enter text to scan: ")
+	//n, err := fmt.Scanln(&name, &category, &price)
+
+	//source := "Lifejacket watersports 264.50"
+	//n, err := fmt.Sscan(source, &name, &category, &price)
+
+	if(err == nil) {
+		Printfln("Scanned %v values", n)
+		Printfln("Name: %v, Category: %v, Price: %.2f", name, category, price)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
+
+/*
+	name := "Kayak"
+	Printfln("Bool: %t", len(name) > 1)
+	Printfln("Bool: %t", len(name) > 100)
+	Printfln("Pointer: %p", &name)
+*/
+
+/*
 	name := "Kayak"
 	Printfln("String: %s", name)
 	Printfln("Charater: %c", []rune(name)[0])
 	Printfln("Unicode: %U", []rune(name)[0])
+*/
 
 /*
 	number := 279.00
