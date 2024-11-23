cf1d8dd2e8892a06fa1cb95538251c593b423fd6
Asanbek09 <samuidnua@gmail.com>
2024-11-23 12:38:50 +0600

    Даты, время и продолжительность

--- Chapter_02/19/project/main.go
+++ Chapter_02/19/project/main.go
@@ -1,11 +1,72 @@
 package main
-import "time"
+
+import (
+	"fmt"
+	"time"
+)
 
 func PrintTime(label string, t *time.Time) {
-	Printfln("%s: Day: %v: Month: %v Year: %v", label, t.Day(), t.Month(), t.Year())
+	//layout := "Day: 02 Month: Jan Year: 2006"
+	fmt.Println(label, t.Format(time.RFC822Z))
+	//Printfln("%s: Day: %v: Month: %v Year: %v", label, t.Day(), t.Month(), t.Year())
 }
 
 func main() {
+	layout := "02 Jan 06 15:04"
+	date := "09 Jun 95 19:30"
+
+	london, lonerr := time.LoadLocation("Europe/London")
+	newyork, nycerr := time.LoadLocation("America/New_York")
+	local, _ := time.LoadLocation("Local")
+
+	if (lonerr == nil && nycerr == nil) {
+		nolocation, _ := time.Parse(layout, date)
+		londonTime, _ := time.ParseInLocation(layout, date, london)
+		newyorkTime, _ := time.ParseInLocation(layout, date, newyork)
+		localTime, _ := time.ParseInLocation(layout, date, local)
+
+		PrintTime("No location:", &nolocation)
+		PrintTime("London:", &londonTime)
+		PrintTime("New York:", &newyorkTime)
+		PrintTime("Local:", &localTime)
+	} else {
+		fmt.Println(lonerr.Error(), nycerr.Error())
+	}
+
+
+	/*
+	layout := "2006-Jan-02"
+	dates :=[]string {
+		"1995-Jun-09",
+		"2015-Jun-02",
+	}
+	for _, d := range dates {
+		time, err := time.Parse(layout, d)
+		if (err == nil) {
+			PrintTime("Parsed:", &time)
+		} else {
+			Printfln("Error: %s", err.Error())
+		}
+	}
+	*/
+
+	/*
+
+	dates := []string {
+		"09 Jun 95 00:00 GMT",
+		"02 Jun 15 00:00 GMT",
+	}
+	for _, d := range dates {
+		time, err := time.Parse(time.RFC822, d)
+		if (err == nil) {
+			PrintTime("Parsed", &time)
+		} else {
+			Printfln("Error: %s", err.Error())
+		}
+	}
+	*/
+
+	/*
 	current := time.Now()
 	specific := time.Date(1995, time.June, 9,0, 0, 0, 0, time.Local)
 	unix := time.Unix(1433228090, 0)
@@ -13,4 +74,5 @@ func main() {
 	PrintTime("Current", &current)
 	PrintTime("Specific", &specific)
 	PrintTime("Unix", &unix)
+	*/
 }
