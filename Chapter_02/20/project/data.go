d8c0acf2d4860b22d9c31f46f15cab2b9261148d
Asanbek09 <samuidnua@gmail.com>
2024-11-25 15:20:23 +0600

    Чтение и запись данных

---
+++ Chapter_02/20/project/data.go
@@ -0,0 +1,35 @@
+package main
+
+import "io"
+
+func GenerateData(writer io.Writer) {
+	data := []byte("Kayak, jacket")
+	writeSize := 4
+	for i := 0; i < len(data); i+= writeSize {
+		end := i + writeSize;
+		if (end > len(data)) {
+			end = len(data)
+		}
+		count, err := writer.Write(data[i: end])
+		Printfln("Wrote %v bytes: %v", count, string(data[i: end]))
+		if(err != nil) {
+			Printfln("Error: %v", err.Error())
+		}
+	}
+}
+
+func ConsumeData(reader io.Reader) {
+	data := make([]byte, 0, 10)
+	slice := make([]byte, 2)
+	for {
+		count, err := reader. Read(slice)
+		if (count > 0) {
+			Printfln("Read data: %v", string(slice[0:count]))
+			data = append(data, slice[0:count]...)
+		}
+		if (err == io.EOF) {
+			break
+		}
+	}
+	Printfln("Read data: %v", string(data))
+}
--- Chapter_02/20/project/main.go
+++ Chapter_02/20/project/main.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"io"
-	"strings"
+	//"strings"
 )
 
-func processData(reader io.Reader) {
+/*
+func processData(reader io.Reader, writer io.Writer) {
+	count, err := io.Copy(writer, reader)
+	if (err == nil) {
+		Printfln("Read %v bytes", count)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
+}
+*/
+
+	/*
 	b := make([]byte, 2)
 	for {
 		count, err := reader.Read(b);
 		if (count > 0) {
+			writer.Write(b[0:count])
 			Printfln("Read: %v bytes: %v", count, string(b[0:count]))
 		}
 		if err == io.EOF {
 			break
 		}
 	}
-}
+	*/
 
 func main() {
+	pipeReader, pipeWriter := io.Pipe()
+	go func() {
+		GenerateData(pipeWriter)
+		pipeWriter.Close()
+	}()
+	ConsumeData(pipeReader)
+
+	/*
 	r := strings.NewReader("Kayak")
-	processData(r)
+	var builder strings.Builder
+	processData(r, &builder)
+	Printfln("String builder contents: %s", builder.String())
+	*/
 }
