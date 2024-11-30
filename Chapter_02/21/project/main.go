d951442a6bb538e005c1e68d4e86644141d1d6eb
Asanbek09 <samuidnua@gmail.com>
2024-11-30 13:21:57 +0600

    Работа с данными JSON

--- Chapter_02/21/project/main.go
+++ Chapter_02/21/project/main.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"encoding/json"
-	"io"
 	"strings"
 )
 
 func main() {
+	
+	reader := strings.NewReader(`true "Hello" 99.99 200`)
+
+	var bval bool
+	var sval string
+	var fpval float64
+	var ival int
+
+	vals := []interface{} {&bval, &sval, &fpval, &ival}
+
+	decoder := json.NewDecoder(reader)
+
+	for i := 0; i < len(vals); i++ {
+		err := decoder.Decode(vals[i])
+		if err != nil {
+			Printfln("Error: %v", err.Error())
+			break
+		}
+	}
+	Printfln("Decoded (%T): %v", bval, bval)
+	Printfln("Decoded (%T): %v", sval, sval)
+	Printfln("Decoded (%T): %v", fpval, fpval)
+	Printfln("Decoded (%T): %v", ival, ival)
+}
+
+
+	/*
 	reader := strings.NewReader(`true "Hello" 99.99 200`)
 	vals := []interface{} { }
 
@@ -37,7 +63,7 @@ func main() {
 			Printfln("Decoded (%T): %v", val, val)
 		}
 	}
-
+	*/
 
 	/*
 	var writer strings.Builder
@@ -111,4 +137,3 @@ func main() {
 
 	fmt.Print(writer.String())
 	*/
-}
