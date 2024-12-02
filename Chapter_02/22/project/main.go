cd5f24e51494fbd3094acd08c120680c59f54cff
Asanbek09 <samuidnua@gmail.com>
2024-12-02 16:21:06 +0600

    Работа с файлами

--- Chapter_02/22/project/main.go
+++ Chapter_02/22/project/main.go
@@ -1,5 +1,6 @@
 package main
 
+
 func main() {
 	for _, p := range Products {
 		Printfln("Product: %v, category: %v, Price: $%.2f", p.Name, p.Category, p.Price)
--- Chapter_02/22/project/readconfig.go
+++ Chapter_02/22/project/readconfig.go
@@ -1,18 +1,33 @@
 package main
 
-import "os"
+import (
+	"os"
+	"encoding/json"
+	"strings"
+)
+
+type ConfigData struct {
+	UserName string
+	AdditionalProducts []Product
+}
+
+var Config ConfigData
 
 func LoadConfig() (err error) {
 	data, err := os.ReadFile("config.json")
-	if err == nil {
-		Printfln(string(data))
+	if (err == nil) {
+		decoder := json.NewDecoder(strings.NewReader(string(data)))
+		err = decoder.Decode(&Config)
 	}
 	return
 }
 
 func init() {
 	err := LoadConfig()
-	if err != nil {
+	if (err != nil) {
 		Printfln("Error Loading Config: %v", err.Error())
+	} else {
+		Printfln("Username: %v", Config.UserName)
+		Products = append(Products, Config.AdditionalProducts...)
 	}
 }
