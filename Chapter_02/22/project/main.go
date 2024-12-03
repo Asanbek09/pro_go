62f1183616c54c1692733ac98f6054696aebcf90
Asanbek09 <samuidnua@gmail.com>
2024-12-03 10:54:43 +0600

    Работа с файлами

--- Chapter_02/22/project/main.go
+++ Chapter_02/22/project/main.go
@@ -1,8 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"time"
+	"os"
+)
 
 func main() {
+	total := 0.0
 	for _, p := range Products {
-		Printfln("Product: %v, category: %v, Price: $%.2f", p.Name, p.Category, p.Price)
+		total += p.Price
+	}
+
+	dataStr := fmt.Sprintf("Time: %v, Total: $%.2f\n", time.Now().Format("Mon 15:05:03"), total)
+
+	err := os.WriteFile("output.txt", []byte(dataStr), 0666)
+	if (err == nil) {
+		fmt.Println("Output file created")
+	} else {
+		Printfln("Error: %v", err.Error())
 	}
 }
+
+
+/*
+for _, p := range Products {
+		Printfln("Product: %v, category: %v, Price: $%.2f", p.Name, p.Category, p.Price)
+	}
+*/
