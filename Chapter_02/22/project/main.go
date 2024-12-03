cd2002962160bbb9bdf8b51973b54057d933540c
Asanbek09 <samuidnua@gmail.com>
2024-12-03 11:09:02 +0600

    Работа с файлами

--- Chapter_02/22/project/main.go
+++ Chapter_02/22/project/main.go
@@ -12,6 +12,24 @@ func main() {
 		total += p.Price
 	}
 
+	dataStr := fmt.Sprintf("Time: %v, Total: $%.2f\n", time.Now().Format("Mon 14:05:09"), total)
+
+	file, err := os.OpenFile("output.txt", os.O_WRONLY | os.O_CREATE | os.O_APPEND, 0666)
+
+	if (err == nil) {
+		defer file.Close()
+		file.WriteString(dataStr)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
+}
+
+/*
+total := 0.0
+	for _, p := range Products {
+		total += p.Price
+	}
+
 	dataStr := fmt.Sprintf("Time: %v, Total: $%.2f\n", time.Now().Format("Mon 15:05:03"), total)
 
 	err := os.WriteFile("output.txt", []byte(dataStr), 0666)
@@ -20,8 +38,7 @@ func main() {
 	} else {
 		Printfln("Error: %v", err.Error())
 	}
-}
-
+*/
 
 /*
 for _, p := range Products {
