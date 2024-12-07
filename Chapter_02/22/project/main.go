9f08b5dc57289ecd903c3b03789c5eb504b96b0f
Asanbek09 <samuidnua@gmail.com>
2024-12-07 12:56:33 +0600

    Работа с файлами

--- Chapter_02/22/project/main.go
+++ Chapter_02/22/project/main.go
@@ -3,12 +3,30 @@ package main
 import (
 	//"fmt"
 	//"time"
-	"os"
 	"encoding/json"
+	"os"
 )
 
 func main() {
-	cheapProducts := []Product {}
+	cheapProducts := []Product{}
+	for _, p := range Products {
+		if p.Price < 100 {
+			cheapProducts = append(cheapProducts, p)
+		}
+	}
+
+	file, err := os.CreateTemp(".", "tempfile-*.json")
+	if err == nil {
+		defer file.Close()
+		encoder := json.NewEncoder(file)
+		encoder.Encode(cheapProducts)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
+}
+
+/*
+cheapProducts := []Product {}
 	for _, p := range Products {
 		if (p.Price < 100) {
 			cheapProducts = append(cheapProducts, p)
@@ -23,7 +41,7 @@ func main() {
 	} else {
 		Printfln("Error: %v", err.Error())
 	}
-}
+*/
 
 /*
 total := 0.0
