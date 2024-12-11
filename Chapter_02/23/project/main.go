81c08661e9fe853d90d963c38a00d232a4984cc8
Asanbek09 <samuidnua@gmail.com>
2024-12-11 21:44:44 +0600

    Использование HTML и текстовых шаблонов

--- Chapter_02/23/project/main.go
+++ Chapter_02/23/project/main.go
@@ -6,10 +6,33 @@ import (
 )
 
 func main() {
-	t, err := template.ParseFiles("templates/template.html")
+	allTemplates, err1 := template.ParseFiles("templates/template.html", "templates/extras.html")
+	if (err1 == nil) {
+		allTemplates.ExecuteTemplate(os.Stdout, "template.html", &Kayak)
+		os.Stdout.WriteString("\n")
+		allTemplates.ExecuteTemplate(os.Stdout, "extras.html", &Kayak)
+	} else {
+		Printfln("Error: %v", err1.Error())
+	}
+}
+
+/*
+t1, err1 := template.ParseFiles("templates/template.html")
+	t2, err2 := template.ParseFiles("templates/extras.html")
+	if (err1 == nil && err2 == nil) {
+		t1.Execute(os.Stdout, &Kayak)
+		os.Stdout.WriteString("\n")
+		t2.Execute(os.Stdout, &Kayak)
+	} else {
+		Printfln("Err: %v %v", err1.Error(), err2.Error())
+	}
+*/
+
+/*
+t, err := template.ParseFiles("templates/template.html")
 	if (err == nil) {
 		t.Execute(os.Stdout, &Kayak)
 	} else {
 		Printfln("Error: %v", err.Error())
 	}
-}
+*/
