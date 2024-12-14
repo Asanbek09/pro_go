724c6af5a3140d51423fe01e6cae9707d0f7df0d
Asanbek09 <samuidnua@gmail.com>
2024-12-14 20:42:18 +0600

    Создание HTTP-серверов

---
+++ Chapter_02/24/project/product.go
@@ -0,0 +1,17 @@
+package main
+
+type Product struct {
+	Name, Category string
+	Price float64
+}
+
+var Products =[]Product {
+	{"kayak", "water sport", 243},
+	{"jacket", "water sport", 48.55},
+	{"ball", "soccer", 17.95},
+	{"flags", "soccer", 47.50},
+	{"stadium", "soccer", 98500},
+	{"cap", "chess", 166},
+	{"chair", "chess", 75},
+	{"bling-bling king", "chess", 1105},
+}
