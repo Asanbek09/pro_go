6d646ffceed332f1c914c2180450a1245bc2f336
Asanbek09 <samudinua@gmail.com>
2024-12-22 17:56:03 +0600

    Создание HTTP-клиентов

--- Chapter_02/25/project/main.go
+++ Chapter_02/25/project/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
-	Printfln("Starting HTTP Server")
-	http.ListenAndServe(":5550", nil)
+	go http.ListenAndServe(":5550", nil)
+	time.Sleep(time.Second)
+
+	response, err := http.Get("http://localhost:5000/html")
+	if (err == nil) {
+		response.Write(os.Stdout)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
 }
