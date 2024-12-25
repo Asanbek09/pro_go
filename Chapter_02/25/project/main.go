059e865343db529289308ff4f7936246d1646dd6
Asanbek09 <samudinua@gmail.com>
2024-12-25 10:09:10 +0600

    Создание HTTP-клиентов

--- Chapter_02/25/project/main.go
+++ Chapter_02/25/project/main.go
@@ -4,16 +4,21 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"io"
 )
 
 func main() {
 	go http.ListenAndServe(":5550", nil)
 	time.Sleep(time.Second)
 
-	response, err := http.Get("http://localhost:5000/html")
-	if (err == nil) {
-		response.Write(os.Stdout)
+	response, err := http.Get("http://localhost:5550/html")
+	if (err == nil && response.StatusCode == http.StatusOK) {
+		data, err := io.ReadAll(response.Body)
+		if (err == nil) {
+			defer response.Body.Close()
+			os.Stdout.Write(data)
+		}
 	} else {
-		Printfln("Error: %v", err.Error())
+		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode	)
 	}
 }
