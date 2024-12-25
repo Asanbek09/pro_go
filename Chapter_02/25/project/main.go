8b6087549c9562f9c16f9033b55ae0803e80003a
Asanbek09 <samudinua@gmail.com>
2024-12-25 14:40:51 +0600

    Создание HTTP-клиентов

--- Chapter_02/25/project/main.go
+++ Chapter_02/25/project/main.go
@@ -7,38 +7,33 @@ import (
 	"io"
 	//"encoding/json"
 	//"strings"
-	//"net/url"
-	"net/http/cookiejar"
-	"fmt"
+	"net/url"
+	//"net/http/cookiejar"
+	//"fmt"
 )
 
 func main() {
 	go http.ListenAndServe(":5550", nil)
 	time.Sleep(time.Second)
 
-	jar, err := cookiejar.New(nil)
+	http.DefaultClient.CheckRedirect = func(req *http.Request, previous []*http.Request) error {
+		if(len(previous) == 3) {
+			url, _ := url.Parse("http://localhost:5550/html")
+			req.URL = url
+		}
+		return nil
+	}
 
-	clients := make([]http.Client, 3)
-	for index, client := range clients {
-		//jar, err := cookiejar.New(nil)
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:5550/redirect1", nil)
+	if (err == nil) {
+		var response *http.Response
+		response, err = http.DefaultClient.Do(req)
 		if (err == nil) {
-			client.Jar = jar
+			io.Copy(os.Stdout, response.Body)
+		} else {
+			Printfln("Request Error: %v", err.Error())
 		}
-
-		for i := 0; i < 3; i ++ {
-			req, err := http.NewRequest(http.MethodGet, "http://localhost:5550/cookie", nil)
-			if(err == nil) {
-				response, err := client.Do(req)
-				if (err == nil && response.StatusCode == http.StatusOK) {
-					fmt.Fprintf(os.Stdout, "Client: %v", index)
-					io.Copy(os.Stdout, response.Body)
-					defer response.Body.Close()
-				} else {
-					Printfln("Request Error: %v", err.Error())
-				}
-			} else {
-				Printfln("Request Init Error: %v", err.Error())
-			}
-		}
+	} else {
+		Printfln("Error: %v", err.Error())
 	}
 }
