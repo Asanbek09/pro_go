545f493861554e375bb3a89e537e9bf0c5958db4
Asanbek09 <samudinua@gmail.com>
2024-12-25 10:21:58 +0600

    Создание HTTP-клиентов

--- Chapter_02/25/project/main.go
+++ Chapter_02/25/project/main.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	"net/http"
-	//"os"
+	"os"
 	"time"
-	//"io"
-	"encoding/json"
+	"io"
+	//"encoding/json"
 )
 
 func main() {
 	go http.ListenAndServe(":5550", nil)
 	time.Sleep(time.Second)
 
-	response, err := http.Get("http://localhost:5550/json")
-	if (err == nil && response.StatusCode == http.StatusOK) {
+	formData := map[string][]string {
+		"name": { "kayak" },
+		"category": { "water sports" },
+		"price": { "259" },
+	}
+
+	response, err := http.PostForm("http://localhost:5550/echo", formData)
+
+	if(err == nil && response.StatusCode == http.StatusOK) {
+		io.Copy(os.Stdout, response.Body)
 		defer response.Body.Close()
-		data := []Product {}
-		err = json.NewDecoder(response.Body).Decode(&data)
-		if (err == nil) {
-			for _, p := range data {
-				Printfln("Name: %v, Price: $%.2f", p.Name, p.Price)
-			}
-		} else {
-			Printfln("Decode error: %v", err.Error())
-		}
 	} else {
 		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
 	}
