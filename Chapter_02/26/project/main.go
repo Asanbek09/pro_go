3addb8e24f41ad6aeffa737b9408fa494907d54e
Asanbek09 <samudinua@gmail.com>
2024-12-27 10:11:12 +0600

    Работа с базами данных

--- Chapter_02/26/project/main.go
+++ Chapter_02/26/project/main.go
@@ -6,9 +6,9 @@ func queryDatabase(db *sql.DB) {
 	rows, err := db.Query("Select * from Products")
 	if (err == nil) {
 		for (rows.Next()) {
-			var id, category int
+			var id, category string
 			var name string
-			var price float64
+			var price string
 			rows.Scan(&id, &name, &category, &price)
 			Printfln("Row: %v %v %v %v", id, name, category, price)
 		}
