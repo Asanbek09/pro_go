a70573149310d56ca78148434881ac60cf5b99d3
Asanbek09 <samudinua@gmail.com>
2024-12-28 15:37:13 +0600

    Работа с базами данных

--- Chapter_02/26/project/main.go
+++ Chapter_02/26/project/main.go
@@ -17,57 +17,30 @@ type Product struct {
 	Price float64
 }
 
-func queryDatabase(db *sql.DB, id int) (p Product) {
-	row := db.QueryRow(`select products.id, products.name, products.price, categories.id as cat_id, 
-	categories.name as catname from products, categories 
-	where products.category = categories.id and products.id = ?`, id)
-	if(row.Err() == nil) {
-		scanErr := row.Scan(&p.Id, &p.Name, &p.Price, &p.Category.Id, &p.Category.Name)
-		if (scanErr != nil) {
-			Printfln("Scan error: %v", scanErr)
-		}
+func queryDatabase(db *sql.DB) (products []Product, err error) {
+	rows, err := db.Query(`select products.id, products.name, products.price, categories.id as "category.id", categories.name as "category.name" 
+	from products, categories where products.category = categories.id`)
+	if (err != nil) {
+		return
 	} else {
-		Printfln("Row error: %v", row.Err().Error())
+		results, err := scanIntoStruct(rows, &Product{})
+		if err == nil {
+			products = (results).([]Product)
+		} else {
+			Printfln("Scanning error: %v", err)
+		}
 	}
 	return
-}
+} 
 
-func insertAndUseCategory(db *sql.DB, name string, productIDs ...int) (err error) {
-	tx, err := db.Begin()
-	updatedFailed := false
-	if (err == nil) {
-		catResult, err := tx.Stmt(insertNewCategory).Exec(name)
-		if (err == nil) {
-			newID, _ := catResult.LastInsertId()
-			preparedStatement := tx.Stmt(changeProductCategory)
-			for _, id := range productIDs {
-				changeResult, err := preparedStatement.Exec(newID, id)
-				if (err == nil) {
-					changes, _ := changeResult.RowsAffected()
-					if (changes == 0) {
-						updatedFailed = true
-						break
-					}
-				}
-			}
-		}
-	}
-	if(err != nil || updatedFailed) {
-		Printfln("Aborting transaction %v", err)
-		tx.Rollback()
-	} else {
-		tx.Commit()
-	}
-	return
-}
 
 func main() {
 	db, err := openDatabase()
 	if (err == nil) {
-		insertAndUseCategory(db, "Category_1", 2)
-		p := queryDatabase(db, 2)
-		Printfln("Product: %v", p)
-		insertAndUseCategory(db, "Category_2", 100)
+		products, _ := queryDatabase(db)
+		for _, p := range products {
+			Printfln("Product: %v", p)
+		}
 		db.Close()
 	} else {
 		panic(err)
