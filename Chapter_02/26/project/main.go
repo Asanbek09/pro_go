6b9c0fda3ecd1ba655455a71893d3862452e8da7
Asanbek09 <samudinua@gmail.com>
2024-12-28 11:25:58 +0600

    Работа с базами данных

--- Chapter_02/26/project/main.go
+++ Chapter_02/26/project/main.go
@@ -14,14 +14,14 @@ type Product struct {
 	Price float64
 }
 
-func queryDatabase(db *sql.DB) []Product {
+func queryDatabase(db *sql.DB, categoryName string) []Product {
 	products := []Product {}
-	rows, err := db.Query(`select products.id, products.name, products.price, categories.id as cat_id, categories.name as catname from products, categories where products.category = categories.id `)
-	if (err == nil) {
-		for(rows.Next()) {
+	rows, err := db.Query(`select products.id, products.name, products.price, categories.id as cat_id, categories.name as catname from products, categories where products.category = categories.id and categories.name=?`, categoryName)
+	if(err == nil) {
+		for (rows.Next()) {
 			p := Product{}
 			scanErr := rows.Scan(&p.Id, &p.Name, &p.Price, &p.Category.Id, &p.Category.Name)
-			if (scanErr == nil) {
+			if(scanErr == nil) {
 				products = append(products, p)
 			} else {
 				Printfln("Scan error: %v", scanErr)
@@ -38,9 +38,12 @@ func main() {
 	//listDrivers()
 	db, err := openDatabase()
 	if(err == nil) {
-		products := queryDatabase(db)
-		for i, p := range products {
-			Printfln("#%v: %v", i, p)
+		for _, cat := range []string {"Soccer", "Watersports"} {
+			Printfln("-----%v Results -----", cat)
+			products := queryDatabase(db, cat)
+			for i, p := range products {
+				Printfln("#%v: %v %v %v", i, p.Name, p.Category, p.Category.Name, p.Price)
+			}
 		}
 		db.Close()
 	} else {
