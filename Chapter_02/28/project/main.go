5397cb4afbdf056fc3e639d1633b7551ad75fcae
Asanbek09 <samudinua@gmail.com>
2025-01-02 20:27:30 +0600

    Использование рефлексии, часть 2

--- Chapter_02/28/project/main.go
+++ Chapter_02/28/project/main.go
@@ -6,16 +6,12 @@ import (
 	//"fmt"
 )
 
-func setValue(arrayOrSlice interface{}, index int, replacement interface{}) {
+func enumerateStrings(arrayOrSlice interface{}) {
 	arrayOrSliceVal := reflect.ValueOf(arrayOrSlice)
-	replacementVal := reflect.ValueOf(replacement)
-	if (arrayOrSliceVal.Kind() == reflect.Slice) {
-		elemVal := arrayOrSliceVal.Index(index)
-		if (elemVal.CanSet()) {
-			elemVal.Set(replacementVal)
+	if (arrayOrSliceVal.Kind() == reflect.Array || arrayOrSliceVal.Kind() == reflect.Slice) && arrayOrSliceVal.Type().Elem().Kind() == reflect.String {
+		for i := 0; i < arrayOrSliceVal.Len(); i++ {
+			Printfln("Element: %v, Value: %v", i, arrayOrSliceVal.Index(i).String())
 		}
-	} else if (arrayOrSliceVal.Kind() == reflect.Ptr && arrayOrSliceVal.Elem().Kind() == reflect.Array && arrayOrSliceVal.Elem().CanSet()) {
-		arrayOrSliceVal.Elem().Index(index).Set(replacementVal)
 	}
 }
 
@@ -27,13 +23,6 @@ func main() {
 	slice := []string {name, city, hobby}
 	array := [3]string {name, city, hobby}
 
-	Printfln("Original Slice: %v", slice)
-	newCity := "Paris"
-	setValue(slice, 1, newCity)
-	Printfln("Modified slice: %v", slice)
-
-	Printfln("Original slice: %v", array)
-	newCity = "Rome"
-	setValue(&array, 1, newCity)
-	Printfln("Modified slice: %v", array)
+	enumerateStrings(slice)
+	enumerateStrings(array)
 }
