2a86d3d06b77f425c9d56228f0a68ab2b2d2319e
Asanbek09 <samudinua@gmail.com>
2025-01-03 10:50:51 +0600

    Использование рефлексии, часть 2

--- Chapter_02/28/project/main.go
+++ Chapter_02/28/project/main.go
@@ -2,37 +2,34 @@ package main
 
 import (
 	"reflect"
-	//"strings"
+	"strings"
 	//"fmt"
 )
 
-func setMap(m interface{}, key interface{}, val interface{}) {
-	mapValue := reflect.ValueOf(m)
-	keyValue := reflect.ValueOf(key)
-	valValue := reflect.ValueOf(val)
-	if (mapValue.Kind() == reflect.Map && mapValue.Type().Key() == keyValue.Type() && mapValue.Type().Elem() == valValue.Type()) {
-		mapValue.SetMapIndex(keyValue, valValue)
-	} else {
-		Printfln("Not a map or mismatched types")
+func createMap(slice interface{}, op func(interface{}) interface{}) interface{} {
+	sliceVal := reflect.ValueOf(slice)
+	if (sliceVal.Kind() == reflect.Slice) {
+		mapType := reflect.MapOf(sliceVal.Type().Elem(), sliceVal.Type().Elem())
+		mapVal := reflect.MakeMap(mapType)
+		for i := 0; i < sliceVal.Len(); i++ {
+			elemVal := sliceVal.Index(i)
+			mapVal.SetMapIndex(elemVal, reflect.ValueOf(op(elemVal.Interface())))
+		}
+		return mapVal.Interface()
 	}
-}
-
-func removeFromMap(m interface{}, key interface{}) {
-	mapValue := reflect.ValueOf(m)
-	keyValue := reflect.ValueOf(key)
-	if (mapValue.Kind() == reflect.Map && mapValue.Type().Key() == keyValue.Type()) {
-		mapValue.SetMapIndex(keyValue, reflect.Value{})
-	}
+	return nil
 }
 
 func main() {
-	pricesMap := map[string]float64 {
-		"kayak": 299, "jacket": 45.98, "soccer ball": 15.69,
+	names := []string {"Alice", "Bob", "Charlie"}
+	reverse := func(val interface{}) interface{} {
+		if str, ok := val.(string); ok {
+			return strings.ToUpper(str)
+		}
+		return val
 	}
-	setMap(pricesMap, "Kayak", 56.00)
-	setMap(pricesMap, "Hat", 25.00)
-	removeFromMap(pricesMap, "jacket")
-	for k, v := range pricesMap {
+	namesMap := createMap(names, reverse).(map[string]string)
+	for k, v := range namesMap {
 		Printfln("Key: %v, Value: %v", k, v)
 	}
 }
