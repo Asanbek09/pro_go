ff4c1c718a56ea0e9613ef6e8317a86d7c05cb66
Asanbek09 <samudinua@gmail.com>
2025-01-03 12:49:03 +0600

    Использование рефлексии, часть 3

--- Chapter_02/29/project/main.go
+++ Chapter_02/29/project/main.go
@@ -6,50 +6,26 @@ import (
 	//"fmt"
 )
 
-func setFieldValue(s interface{}, newVals map[string]interface{}) {
-	structValue := reflect.ValueOf(s)
-	if (structValue.Kind() == reflect.Ptr && structValue.Elem().Kind() == reflect.Struct) {
-		for name, newValue := range newVals {
-			fieldVal := structValue.Elem().FieldByName(name)
-			if (fieldVal.CanSet()) {
-				fieldVal.Set(reflect.ValueOf(newValue))
-			} else if (fieldVal.CanAddr()) {
-				ptr := fieldVal.Addr()
-				if (ptr.CanSet()) {
-					ptr.Set(reflect.ValueOf(newValue))
-				} else {
-					Printfln("Cannot set field via pointer")
-				}
+func inspectFuncType(f interface{}) {
+	funcType := reflect.TypeOf(f)
+	if (funcType.Kind() == reflect.Func) {
+		Printfln("Function parameters: %v", funcType.NumIn())
+		for i := 0; i < funcType.NumIn(); i++ {
+			paramType := funcType.In(i)
+			if (i < funcType.NumIn() -1) {
+				Printfln("Parameter #%v, Type: %v", i, paramType)
 			} else {
-				Printfln("cannot set field")
+				Printfln("Parameter #%v, Type: %v, Variadic: %v", i, paramType, funcType.IsVariadic())
 			}
 		}
-	} else {
-		Printfln("Not a pointer to a struct")
-	}
-}
-
-func getFieldValues(s interface{}) {
-	structValue := reflect.ValueOf(s)
-	if structValue.Kind() == reflect.Struct {
-		for i := 0; i < structValue.NumField(); i++ {
-			fieldType := structValue.Type().Field(i)
-			fieldVal := structValue.Field(i)
-			Printfln("Name: %v, Type: %v, Value: %v", fieldType.Name, fieldType.Type, fieldVal)
+		Printfln("Function results: %v", funcType.NumOut())
+		for i := 0; i < funcType.NumOut(); i++ {
+			resultType := funcType.Out(i)
+			Printfln("Result #%v, Type: %v", i, resultType)
 		}
-	} else {
-		Printfln("Not a struct")
 	}
 }
 
 func main() {
-	product := Product {Name: "Kayak", Category: "Water sports", Price: 277}
-	customer := Customer {Name: "Alice", City: "Boston"}
-	purchase := Purchase {Customer: customer, Product: product, Total: 290, taxRate: 20}
-
-	setFieldValue(&purchase, map[string]interface{} {
-		"City": "London", "Category": "Boats", "Total": 215.20,
-	})
-
-	getFieldValues(purchase)
+	inspectFuncType(Find)
 }
