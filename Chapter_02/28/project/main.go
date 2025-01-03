e6cc9eb8c6b96630676371c293cba2537055201f
Asanbek09 <samudinua@gmail.com>
2025-01-03 11:25:18 +0600

    Использование рефлексии, часть 2

--- Chapter_02/28/project/main.go
+++ Chapter_02/28/project/main.go
@@ -6,29 +6,24 @@ import (
 	//"fmt"
 )
 
-func inspectStructs(structs ...interface{}) {
-	for _, s := range structs {
-		structType := reflect.TypeOf(s)
-		if (structType.Kind() == reflect.Struct) {
-			inspectStructType([]int {}, structType)
+func describeField(s interface{}, fieldName string) {
+	structType := reflect.TypeOf(s)
+	field, found := structType.FieldByName(fieldName)
+	if (found) {
+		Printfln("Found: %v, Type: %v, Index: %v", field.Name, field.Type, field.Index)
+		index := field.Index
+		for len(index) > 1 {
+			index = index[0: len(index) -1]
+			field = structType.FieldByIndex(index)
+			Printfln("Parent: %v, Type: %v, Index: %v", field.Name, field.Type, field.Index)
 		}
+		Printfln("Top-level type: %v", structType)
+	} else {
+		Printfln("Field %v not found", fieldName)
 	}
-}
-
-func inspectStructType(baseIndex []int, structType reflect.Type) {
-	Printfln("--- Struct Type: %v", structType)
-	for i := 0; i < structType.NumField(); i++ {
-		field := structType.Field(i)
-		Printfln("Field %v: Name: %v, Type: %v, Exported: %v", field.Index, field.Name, field.Type, field.PkgPath == "")
-		if (field.Type.Kind() == reflect.Struct) {
-			field := structType.FieldByIndex(field.Index)
-			inspectStructType(field.Index, field.Type)
-		}
-	}
-	Printfln("--- End struct type: %v", structType)
 }
 
 
 func main() {
-	inspectStructs(Purchase{})
+	describeField(Purchase{}, "Price")
 }
