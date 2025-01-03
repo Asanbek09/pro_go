4459cba1423664e63c3215e4194f34435a0f0ed6
Asanbek09 <samudinua@gmail.com>
2025-01-03 19:56:32 +0600

    Использование тестирования

--- Chapter_02/31/project/simple_test.go
+++ Chapter_02/31/project/simple_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"sort"
+)
 
 func TestSum(t *testing.T) {
 	testValues := []int {10, 20, 30}
@@ -8,5 +11,13 @@ func TestSum(t *testing.T) {
 	expected := 60
 	if (sum != expected) {
 		t.Fatalf("Expected %v, Got %v", expected, sum)
+	}
+}
+
+func TestSort(t *testing.T) {
+	testValues := []int {1, 23, 49, 93, 2, 5, 43, 9}
+	sorted, _ := sortAndTotal(testValues)
+	if (!sort.IntsAreSorted(sorted)) {
+		t.Fatalf("Unsorted data: %v", sorted)
 	}
 }
