603d1cebe8b01391e27960360ea2b04f6372370c
Asanbek09 <samudinua@gmail.com>
2025-01-03 15:41:29 +0600

    Использование рефлексии, часть 3

--- Chapter_02/29/project/main.go
+++ Chapter_02/29/project/main.go
@@ -6,21 +6,18 @@ import (
 	//"fmt"
 )
 
-func executeFirstVoidMethods(s interface{}) {
-	sVal := reflect.ValueOf(s)
-	for i := 0; i < sVal.NumMethod(); i++ {
-		method := sVal.Method(i)
-		if (method.Type().NumIn() == 0) {
-			results := method.Call([]reflect.Value{})
-			Printfln("Type: %v, Method: %v, Results: %v", sVal.Type(), sVal.Type().Method(i).Name, results)
-			break
-		} else {
-			Printfln("Skipping method %v %v", sVal.Type().Method(i).Name, method.Type().NumIn())
+func checkImplementation(check interface{}, targets ...interface{}) {
+	checkType := reflect.TypeOf(check)
+	if (checkType.Kind() == reflect.Ptr && checkType.Elem().Kind() == reflect.Interface) {
+		checkType := checkType.Elem()
+		for _, target := range targets {
+			targetType := reflect.TypeOf(target)
+			Printfln("Type %v implements %v : %v", targetType, checkType, targetType.Implements(checkType))
 		}
 	}
 }
 
-
 func main() {
-	executeFirstVoidMethods(&Product {Name: "kayak", Price: 268})
+	currencyItemType := (*CurrencyItem)(nil)
+	checkImplementation(currencyItemType, Product{}, &Product{}, &Purchase{})
 }
