87a31425158502a762429b9f2529fcab2dc90550
Asanbek09 <samudinua@gmail.com>
2025-01-03 21:19:17 +0600

    Удаление установки из теста

--- Chapter_02/31/project/Benchmark_test.go
+++ Chapter_02/31/project/Benchmark_test.go
@@ -10,8 +10,11 @@ func BenchmarkSort(b *testing.B) {
 	rand.Seed(time.Now().UnixNano())
 	size := 250
 	data := make([]int, size)
+	b.ResetTimer()
 	for i := 0; i < size; i++ {
+		b.StopTimer()
 		data[i] = rand.Int()
 	}
+	b.StartTimer()
 	sortAndTotal(data)
 }
