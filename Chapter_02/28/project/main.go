d515c60d5d0905c28f576d9522bbbeb61caea08d
Asanbek09 <samudinua@gmail.com>
2025-01-03 11:20:06 +0600

    Использование рефлексии, часть 2

--- Chapter_02/28/project/main.go
+++ Chapter_02/28/project/main.go
@@ -10,16 +10,20 @@ func inspectStructs(structs ...interface{}) {
 	for _, s := range structs {
 		structType := reflect.TypeOf(s)
 		if (structType.Kind() == reflect.Struct) {
-			inspectStructType(structType)
+			inspectStructType([]int {}, structType)
 		}
 	}
 }
 
-func inspectStructType(structType reflect.Type) {
+func inspectStructType(baseIndex []int, structType reflect.Type) {
 	Printfln("--- Struct Type: %v", structType)
 	for i := 0; i < structType.NumField(); i++ {
 		field := structType.Field(i)
 		Printfln("Field %v: Name: %v, Type: %v, Exported: %v", field.Index, field.Name, field.Type, field.PkgPath == "")
+		if (field.Type.Kind() == reflect.Struct) {
+			field := structType.FieldByIndex(field.Index)
+			inspectStructType(field.Index, field.Type)
+		}
 	}
 	Printfln("--- End struct type: %v", structType)
 }
