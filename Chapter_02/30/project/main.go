84181c902834215b1da58f83e230f0405819af9e
Asanbek09 <samudinua@gmail.com>
2025-01-03 17:38:27 +0600

    Использование взаимного исключения

--- Chapter_02/30/project/main.go
+++ Chapter_02/30/project/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"sync"
+	"time"
 )
 
 var waitGroup = sync.WaitGroup{}
 
 func doSum(count int, val *int) {
+	time.Sleep(time.Second)
 	for i := 0; i < count; i ++ {
 		*val++
 	}
@@ -16,8 +18,11 @@ func doSum(count int, val *int) {
 func main() {
 	counter := 0
 
-	waitGroup.Add(1)
-	go doSum(5000, &counter)
+	numRoutines := 3
+	waitGroup.Add(numRoutines)
+	for i := 0; i < numRoutines; i ++ {
+		go doSum(5000, &counter)
+	}
 	waitGroup.Wait()
 	Printfln("Total: %v", counter)
 }
