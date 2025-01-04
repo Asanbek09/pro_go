c3e1253fa261c38bf96930a69cb8c9ce2dd3fe79
Asanbek09 <samudinua@gmail.com>
2025-01-04 16:48:48 +0600

    Регистрация и использование сервисов

--- Chapter_03/platform/main.go
+++ Chapter_03/platform/main.go
@@ -4,6 +4,7 @@ import (
 	//"fmt"
 	"platform/config"
 	"platform/logging"
+	"platform/services"
 )
 
 func writeMessage(logger logging.Logger, cfg config.Configuration) {
@@ -21,12 +22,13 @@ func writeMessage(logger logging.Logger, cfg config.Configuration) {
 }
 
 func main() {
+	services.RegisterDefaultServices()
+
     var cfg config.Configuration
-    var err error
-    cfg, err = config.Load("config.json")
-    if (err != nil) {
-		panic(err)
-	}
-    var logger logging.Logger = logging.NewDefaultLogger(cfg)
-    writeMessage(logger, cfg)
+    services.GetService(&cfg)
+
+	var logger logging.Logger
+	services.GetService(&logger)
+
+	writeMessage(logger, cfg)
 }
