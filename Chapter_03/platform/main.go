4f5025cafc9e7fcfbed17c8020ba048f2aa8af55
Asanbek09 <samudinua@gmail.com>
2025-01-04 16:58:45 +0600

    Добавление поддержки для вызова функций

--- Chapter_03/platform/main.go
+++ Chapter_03/platform/main.go
@@ -24,11 +24,11 @@ func writeMessage(logger logging.Logger, cfg config.Configuration) {
 func main() {
 	services.RegisterDefaultServices()
 
-    var cfg config.Configuration
-    services.GetService(&cfg)
+    //var cfg config.Configuration
+    //services.GetService(&cfg)
 
-	var logger logging.Logger
-	services.GetService(&logger)
+	//var logger logging.Logger
+	//services.GetService(&logger)
 
-	writeMessage(logger, cfg)
+	services.Call(writeMessage)
 }
