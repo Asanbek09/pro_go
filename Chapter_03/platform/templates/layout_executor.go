5978568c462599639391bbfa71c79289038eda3a
Asanbek09 <samudinua@gmail.com>
2025-01-05 14:24:24 +0600

    Вызов обработчиков запросов из шаблонов

--- Chapter_03/platform/templates/layout_executor.go
+++ Chapter_03/platform/templates/layout_executor.go
@@ -1,20 +1,27 @@
 package templates
 
 import (
+	"html/template"
 	"io"
 	"strings"
-	"html/template"
 )
 
 type LayoutTemplateProcessor struct {}
 
+var emptyFunc = func(handlerName, methodName string, args ...interface{}) interface{} {return ""}
+
 func(proc *LayoutTemplateProcessor) ExecTemplate(writer io.Writer, name string, data interface{}) (err error) {
+	return proc.ExecTemplateWithFunc(writer, name, data, emptyFunc)
+}
+
+func(proc *LayoutTemplateProcessor) ExecTemplateWithFunc(writer io.Writer, name string, data interface{}, handlerFunc InvokeHandlerFunc) (err error) {
 	var sb strings.Builder
 	layoutName := ""
 	localTemplates := getTemplates()
 	localTemplates.Funcs(map[string]interface{} {
 		"body": insertBodyWrapper(&sb),
 		"layout": setLayoutWrapper(&layoutName),
+		"handler": handlerFunc,
 	})
 	err = localTemplates.ExecuteTemplate(&sb, name, data)
 	if (layoutName != "") {
